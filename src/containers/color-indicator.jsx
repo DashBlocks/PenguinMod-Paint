b7e766d8028c8f1cc88c35a195b40a4ac3f59f8f
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-01 15:10:13 +0500

    Update color-indicator.jsx

--- src/containers/color-indicator.jsx
+++ src/containers/color-indicator.jsx
@@ -39,32 +39,6 @@ const makeColorIndicator = (label, isStroke) => {
             }
         }
         handleChangeColor (newColor) {
-            // Stroke-selector-specific logic: if we change the stroke color from "none" to something visible, ensure
-            // there's a nonzero stroke width. If we change the stroke color to "none", set the stroke width to zero.
-            if (isStroke) {
-
-                // Whether the old color style in this color indicator was null (completely transparent).
-                // If it's a solid color, this means that the first color is null.
-                // If it's a gradient, this means both colors are null.
-                const oldStyleWasNull = this.props.gradientType === GradientTypes.SOLID ?
-                    this.props.color === null :
-                    this.props.color === null && this.props.color2 === null;
-
-                const otherColor = this.props.colorIndex === 1 ? this.props.color : this.props.color2;
-                // Whether the new color style in this color indicator is null.
-                const newStyleIsNull = this.props.gradientType === GradientTypes.SOLID ?
-                    newColor === null :
-                    newColor === null && otherColor === null;
-
-                if (oldStyleWasNull && !newStyleIsNull) {
-                    this._hasChanged = applyStrokeWidthToSelection(1, this.props.textEditTarget) || this._hasChanged;
-                    this.props.onChangeStrokeWidth(1);
-                } else if (!oldStyleWasNull && newStyleIsNull) {
-                    this._hasChanged = applyStrokeWidthToSelection(0, this.props.textEditTarget) || this._hasChanged;
-                    this.props.onChangeStrokeWidth(0);
-                }
-            }
-
             const formatIsBitmap = isBitmap(this.props.format);
             // Apply color and update redux, but do not update svg until picker closes.
             const isDifferent = applyColorToSelection(
@@ -96,7 +70,7 @@ const makeColorIndicator = (label, isStroke) => {
             if (this.props.gradientType === GradientTypes.SOLID && gradientType !== GradientTypes.SOLID) {
                 // Generate color 2 and change to the 2nd swatch when switching from solid to gradient
                 if (!hasSelectedItems) {
-                    this.props.onChangeColor(generateSecondaryColor(this.props.color), 1);
+                    this.props.onChangeColor(generateSecondaryColor(this.props.stops[0].color), 1);
                 }
                 this.props.onChangeColorIndex(1);
             }
@@ -122,12 +96,9 @@ const makeColorIndicator = (label, isStroke) => {
                 this.props.setSelectedItems(this.props.format);
                 this._hasChanged = this._hasChanged || isDifferent;
             } else {
-                let color1 = this.props.color;
-                let color2 = this.props.color2;
-                color1 = color1 === null || color1 === MIXED ? color1 : parseColor(color1).hex;
-                color2 = color2 === null || color2 === MIXED ? color2 : parseColor(color2).hex;
-                this.props.onChangeColor(color1, 1);
-                this.props.onChangeColor(color2, 0);
+                const newColors = this.props.stops.toReversed()
+                    .map((stop) => stop.color === MIXED ? stop.color : parseColor(stop.color).hex);
+                newColors.forEach((color, i) => this.props.onChangeColor(color, i));
             }
         }
         render () {
@@ -148,8 +119,6 @@ const makeColorIndicator = (label, isStroke) => {
     ColorIndicator.propTypes = {
         colorIndex: PropTypes.number.isRequired,
         disabled: PropTypes.bool.isRequired,
-        color: PropTypes.string,
-        color2: PropTypes.string,
         colorModalVisible: PropTypes.bool.isRequired,
         fillBitmapShapes: PropTypes.bool.isRequired,
         format: PropTypes.oneOf(Object.keys(Formats)),
@@ -163,6 +132,10 @@ const makeColorIndicator = (label, isStroke) => {
         onCloseColor: PropTypes.func.isRequired,
         onUpdateImage: PropTypes.func.isRequired,
         setSelectedItems: PropTypes.func.isRequired,
+        stops: PropTypes.arrayOf(PropTypes.shape({
+            color: PropTypes.string,
+            offset: PropTypes.number
+        })),
         textEditTarget: PropTypes.number
     };
 
