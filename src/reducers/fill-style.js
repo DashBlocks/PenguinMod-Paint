0c2c529fbfc66ce81081f08fbccc39cfc446facf
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-01 21:54:20 +0500

    fill-style.js: Add new action

--- src/reducers/fill-style.js
+++ src/reducers/fill-style.js
@@ -1,11 +1,13 @@
 import makeColorStyleReducer from '../lib/make-color-style-reducer';
 
+const ADD_OTHER_FILL_STOP = 'scratch-paint/fill-style/ADD_OTHER_FILL_STOP';
 const CHANGE_FILL_COLOR = 'scratch-paint/fill-style/CHANGE_FILL_COLOR';
 const CHANGE_FILL_GRADIENT_TYPE = 'scratch-paint/fill-style/CHANGE_FILL_GRADIENT_TYPE';
 const CLEAR_FILL_GRADIENT = 'scratch-paint/fill-style/CLEAR_FILL_GRADIENT';
 const DEFAULT_COLOR = '#9966FF';
 
 const reducer = makeColorStyleReducer({
+    addOtherStopAction: ADD_OTHER_FILL_STOP,
     changeColorAction: CHANGE_FILL_COLOR,
     changeGradientTypeAction: CHANGE_FILL_GRADIENT_TYPE,
     clearGradientAction: CLEAR_FILL_GRADIENT,
@@ -15,6 +17,14 @@ const reducer = makeColorStyleReducer({
 });
 
 // Action creators ==================================
+const addOtherFillStop = function (stopColor, index) {
+    return {
+        type: ADD_OTHER_FILL_STOP,
+        color: stopColor,
+        index
+    };
+};
+
 const changeFillColor = function (fillColor, index) {
     return {
         type: CHANGE_FILL_COLOR,
@@ -38,6 +48,7 @@ const clearFillGradient = function () {
 
 export {
     reducer as default,
+    addOtherFillStop,
     changeFillColor,
     changeFillGradientType,
     clearFillGradient,
