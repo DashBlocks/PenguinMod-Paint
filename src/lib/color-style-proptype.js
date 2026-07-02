9c16fa6cba721639263618a6cfdc00c6d18a3c6c
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-02 12:40:42 +0500

    color-style-proptype.js: ⬆ Update

--- src/lib/color-style-proptype.js
+++ src/lib/color-style-proptype.js
@@ -3,7 +3,9 @@ import {PropTypes} from 'prop-types';
 import GradientTypes from './gradient-types';
 
 export default PropTypes.shape({
-    primary: PropTypes.string,
-    secondary: PropTypes.string,
+    stops: PropTypes.arrayOf(PropTypes.shape({
+        color: PropTypes.string,
+        offset: PropTypes.number
+    })),
     gradientType: PropTypes.oneOf(Object.keys(GradientTypes)).isRequired
 });
