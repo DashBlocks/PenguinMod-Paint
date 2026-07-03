68de8a4e57636560a1910000564ef5db54d78265
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-03 12:42:44 +0500

    color-picker.jsx: 🔧 Fix?

--- src/containers/color-picker.jsx
+++ src/containers/color-picker.jsx
@@ -74,7 +74,7 @@ class ColorPicker extends React.Component {
     }
     componentWillReceiveProps (newProps) {
         const color = this.props.stops[newProps.colorIndex]?.color;
-        const newColor = newProps.stops[newProps.colorIndex].color;
+        const newColor = newProps.stops[newProps.colorIndex]?.color;
         const colorSetByEyedropper = this.props.isEyeDropping && color !== newColor;
         if (colorSetByEyedropper || this.props.colorIndex !== newProps.colorIndex) {
             const hsv = this.getHsv(newColor);
