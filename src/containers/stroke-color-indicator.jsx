0a135e10901e4fb2203ae832424bac96a8b9046d
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-03 09:44:20 +0500

    stroke-color-indicator.jsx: ⬆ Update

--- src/containers/stroke-color-indicator.jsx
+++ src/containers/stroke-color-indicator.jsx
@@ -2,7 +2,11 @@ import {connect} from 'react-redux';
 import {defineMessages} from 'react-intl';
 
 import {changeColorIndex} from '../reducers/color-index';
-import {addOtherStrokeStop, changeStrokeColor, changeStrokeGradientType} from '../reducers/stroke-style';
+import {addOtherStrokeStop,
+    changeStrokeColor,
+    changeStrokeGradientType,
+    moveStrokeStop,
+    removeStrokeStop} from '../reducers/stroke-style';
 import {changeStrokeWidth} from '../reducers/stroke-width';
 import {openStrokeColor, closeStrokeColor} from '../reducers/modals';
 import {getSelectedLeafItems} from '../helper/selection';
@@ -59,6 +63,12 @@ const mapDispatchToProps = dispatch => ({
     },
     onChangeGradientType: gradientType => {
         dispatch(changeStrokeGradientType(gradientType));
+    },
+    onMoveStop: (offset, index) => {
+        dispatch(moveStrokeStop(offset, index));
+    },
+    onRemoveStop: (index) => {
+        dispatch(removeStrokeStop(index));
     },
     setSelectedItems: format => {
         dispatch(setSelectedItems(getSelectedLeafItems(), isBitmap(format)));
