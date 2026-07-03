12bc92dc2f47837caf2f51c082862dc964aed172
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-03 10:31:49 +0500

    fill-mode.jsx: 🔧 Fixes for fill mode. Part 1

--- src/containers/fill-mode.jsx
+++ src/containers/fill-mode.jsx
@@ -7,7 +7,7 @@ import GradientTypes from '../lib/gradient-types';
 import FillTool from '../helper/tools/fill-tool';
 import {generateSecondaryColor, MIXED} from '../helper/style-path';
 
-import {changeFillColor, changeFillColor2, DEFAULT_COLOR} from '../reducers/fill-style';
+import {addOtherFillStop, changeFillColor, DEFAULT_COLOR} from '../reducers/fill-style';
 import {changeMode} from '../reducers/modes';
 import {clearSelectedItems} from '../reducers/selected-items';
 import {clearSelection} from '../helper/selection';
@@ -31,11 +31,8 @@ class FillMode extends React.Component {
     }
     componentWillReceiveProps (nextProps) {
         if (this.tool) {
-            if (nextProps.fillColor !== this.props.fillColor) {
-                this.tool.setFillColor(nextProps.fillColor);
-            }
-            if (nextProps.fillColor2 !== this.props.fillColor2) {
-                this.tool.setFillColor2(nextProps.fillColor2);
+            if (nextProps.stops !== this.props.stops) {
+                this.tool.setStops(nextProps.stops);
             }
             if (nextProps.hoveredItemId !== this.props.hoveredItemId) {
                 this.tool.setPrevHoveredItemId(nextProps.hoveredItemId);
@@ -63,32 +60,36 @@ class FillMode extends React.Component {
         clearSelection(this.props.clearSelectedItems);
 
         // Force the default fill color if fill is MIXED
-        let fillColor = this.props.fillColor;
-        if (this.props.fillColor === MIXED) {
-            fillColor = DEFAULT_COLOR;
+        let stops = this.props.stops;
+        if (this.props.stops[0].color === MIXED) {
+            stops = [{
+                color: DEFAULT_COLOR,
+                offset: 0
+            }];
             this.props.onChangeFillColor(DEFAULT_COLOR, 0);
         }
         const gradientType = this.props.fillModeGradientType ?
             this.props.fillModeGradientType : this.props.fillStyleGradientType;
-        let fillColor2 = this.props.fillColor2;
         if (gradientType !== this.props.fillStyleGradientType) {
             if (this.props.fillStyleGradientType === GradientTypes.SOLID) {
-                fillColor2 = generateSecondaryColor(fillColor);
-                this.props.onChangeFillColor(fillColor2, 1);
+                const generatedColor = generateSecondaryColor(fillColor);
+                stops = stops.append({
+                    color: generatedColor,
+                    offset: 1
+                });
+                this.props.onAddOtherStop(generatedColor, 0);
+            }
+            if (gradientType === GradientTypes.SOLID) {
+                stops = [stops[0]];
             }
             this.props.changeGradientType(gradientType);
-        }
-        if (this.props.fillColor2 === MIXED) {
-            fillColor2 = generateSecondaryColor(fillColor);
-            this.props.onChangeFillColor(fillColor2, 1);
         }
         this.tool = new FillTool(
             this.props.setHoveredItem,
             this.props.clearHoveredItem,
             this.props.onUpdateImage
         );
-        this.tool.setFillColor(fillColor);
-        this.tool.setFillColor2(fillColor2);
+        this.tool.setStops(stops);
         this.tool.setGradientType(gradientType);
         this.tool.setPrevHoveredItemId(this.props.hoveredItemId);
         this.tool.activate();
@@ -112,25 +113,27 @@ FillMode.propTypes = {
     changeGradientType: PropTypes.func.isRequired,
     clearHoveredItem: PropTypes.func.isRequired,
     clearSelectedItems: PropTypes.func.isRequired,
-    fillColor: PropTypes.string,
-    fillColor2: PropTypes.string,
     fillStyleGradientType: PropTypes.oneOf(Object.keys(GradientTypes)).isRequired,
     fillModeGradientType: PropTypes.oneOf(Object.keys(GradientTypes)),
     handleMouseDown: PropTypes.func.isRequired,
     hoveredItemId: PropTypes.number,
     isFillModeActive: PropTypes.bool.isRequired,
+    onAddOtherStop: PropTypes.func.isRequired,
     onChangeFillColor: PropTypes.func.isRequired,
     onUpdateImage: PropTypes.func.isRequired,
-    setHoveredItem: PropTypes.func.isRequired
+    setHoveredItem: PropTypes.func.isRequired,
+    stops: PropTypes.arrayOf(PropTypes.shape({
+        color: PropTypes.string,
+        offset: PropTypes.number
+    }))
 };
 
 const mapStateToProps = state => ({
     fillModeGradientType: state.scratchPaint.fillMode.gradientType, // Last user-selected gradient type
-    fillColor: state.scratchPaint.color.fillColor.primary,
-    fillColor2: state.scratchPaint.color.fillColor.secondary,
     fillStyleGradientType: state.scratchPaint.color.fillColor.gradientType, // Selected item(s)' gradient type
     hoveredItemId: state.scratchPaint.hoveredItemId,
-    isFillModeActive: state.scratchPaint.mode === Modes.FILL
+    isFillModeActive: state.scratchPaint.mode === Modes.FILL,
+    stops: state.scratchPaint.color.fillColor.stops,
 });
 const mapDispatchToProps = dispatch => ({
     setHoveredItem: hoveredItemId => {
@@ -148,12 +151,11 @@ const mapDispatchToProps = dispatch => ({
     handleMouseDown: () => {
         dispatch(changeMode(Modes.FILL));
     },
+    onAddOtherStop: (stopColor, index) => {
+        dispatch(addOtherFillStop(stopColor, index));
+    },
     onChangeFillColor: (fillColor, index) => {
-        if (index === 0) {
-            dispatch(changeFillColor(fillColor));
-        } else if (index === 1) {
-            dispatch(changeFillColor2(fillColor));
-        }
+        dispatch(changeFillColor(fillColor, index));
     }
 });
 
