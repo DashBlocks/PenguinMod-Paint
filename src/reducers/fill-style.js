54b58e57d689b6ab306bd81dffe0e208f02a445e
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-03 09:27:12 +0500

    fill-style.js: ✨ Add 2 new actions

--- src/reducers/fill-style.js
+++ src/reducers/fill-style.js
@@ -5,6 +5,8 @@ const CHANGE_FILL_COLOR = 'scratch-paint/fill-style/CHANGE_FILL_COLOR';
 const CHANGE_FILL_GRADIENT_TYPE = 'scratch-paint/fill-style/CHANGE_FILL_GRADIENT_TYPE';
 const CLEAR_FILL_GRADIENT = 'scratch-paint/fill-style/CLEAR_FILL_GRADIENT';
 const DEFAULT_COLOR = '#9966FF';
+const MOVE_FILL_STOP = 'scratch-paint/fill-style/MOVE_FILL_STOP';
+const REMOVE_FILL_STOP = 'scratch-paint/fill-style/REMOVE_FILL_STOP';
 
 const reducer = makeColorStyleReducer({
     addOtherStopAction: ADD_OTHER_FILL_STOP,
@@ -12,6 +14,8 @@ const reducer = makeColorStyleReducer({
     changeGradientTypeAction: CHANGE_FILL_GRADIENT_TYPE,
     clearGradientAction: CLEAR_FILL_GRADIENT,
     defaultColor: DEFAULT_COLOR,
+    moveStopAction: MOVE_FILL_STOP,
+    removeStopAction: REMOVE_FILL_STOP,
     selectionColorKey: 'fillColor',
     selectionGradientTypeKey: 'fillGradientType'
 });
@@ -46,12 +50,29 @@ const clearFillGradient = function () {
     };
 };
 
+const moveFillStop = function (offset, index) {
+    return {
+        type: MOVE_FILL_STOP,
+        offset,
+        index
+    };
+};
+
+const removeFillStop = function (index) {
+    return {
+        type: REMOVE_FILL_STOP,
+        index
+    };
+};
+
 export {
     reducer as default,
     addOtherFillStop,
     changeFillColor,
     changeFillGradientType,
     clearFillGradient,
+    moveFillStop,
+    removeFillStop,
     DEFAULT_COLOR,
     CHANGE_FILL_GRADIENT_TYPE
 };
