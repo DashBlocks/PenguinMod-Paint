50a598a5d6848ee2d94e508239b3d3767e1c46f8
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-03 09:34:39 +0500

    stroke-style.js: ✨ Add 2 new actions

--- src/reducers/stroke-style.js
+++ src/reducers/stroke-style.js
@@ -1,10 +1,13 @@
 import makeColorStyleReducer from '../lib/make-color-style-reducer';
+import GradientTypes from '../lib/gradient-types';
 
 const ADD_OTHER_STROKE_STOP = 'scratch-paint/stroke-style/ADD_OTHER_STROKE_STOP';
 const CHANGE_STROKE_COLOR = 'scratch-paint/stroke-style/CHANGE_STROKE_COLOR';
 const CHANGE_STROKE_GRADIENT_TYPE = 'scratch-paint/stroke-style/CHANGE_STROKE_GRADIENT_TYPE';
 const CLEAR_STROKE_GRADIENT = 'scratch-paint/stroke-style/CLEAR_STROKE_GRADIENT';
 const DEFAULT_COLOR = '#000000';
+const MOVE_STROKE_STOP = 'scratch-paint/stroke-style/MOVE_STROKE_STOP';
+const REMOVE_STROKE_STOP = 'scratch-paint/stroke-style/REMOVE_STROKE_STOP';
 
 import {CHANGE_STROKE_WIDTH} from './stroke-width';
 
@@ -14,26 +17,24 @@ const reducer = makeColorStyleReducer({
     changeGradientTypeAction: CHANGE_STROKE_GRADIENT_TYPE,
     clearGradientAction: CLEAR_STROKE_GRADIENT,
     defaultColor: DEFAULT_COLOR,
+    moveStopAction: MOVE_STROKE_STOP,
+    removeStopAction: REMOVE_STROKE_STOP,
     selectionColorKey: 'strokeColor',
     selectionGradientTypeKey: 'strokeGradientType'
 });
 
-// This is mostly the same as the generated reducer, but with one piece of extra logic to set the color to null when the
+// This is mostly the same as the generated reducer, but with one piece of extra logic to set the transparent color when the
 // stroke width is set to 0.
 // https://redux.js.org/recipes/structuring-reducers/reusing-reducer-logic
 const strokeReducer = function (state, action) {
     if (action.type === CHANGE_STROKE_WIDTH && Math.max(action.strokeWidth, 0) === 0) {
-        // TODO: this preserves the gradient type when you change the stroke width to 0.
-        // Alternatively, we could set gradientType to SOLID instead of setting secondary to null, but since
-        // the stroke width is automatically set to 0 as soon as a "null" color is detected (including a gradient for
-        // which both colors are null), that would change the gradient type back to solid if you selected null for both
-        // gradient colors.
         return {
             ...state,
             stops: [{
                 color: 'rgba(0,0,0,0)',
                 offset: 0
-            }]
+            }],
+            gradientType: GradientTypes.SOLID
         };
     }
 
@@ -70,12 +71,29 @@ const clearStrokeGradient = function () {
     };
 };
 
+const moveStrokeStop = function (offset, index) {
+    return {
+        type: MOVE_STROKE_STOP,
+        offset,
+        index
+    };
+};
+
+const removeStrokeStop = function (index) {
+    return {
+        type: REMOVE_STROKE_STOP,
+        index
+    };
+};
+
 export {
     strokeReducer as default,
     addOtherStrokeStop,
     changeStrokeColor,
     changeStrokeGradientType,
     clearStrokeGradient,
+    moveStrokeStop,
+    removeStrokeStop,
     DEFAULT_COLOR,
     CHANGE_STROKE_GRADIENT_TYPE
 };
