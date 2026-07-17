afc24dc9cfc1a779e12b880d3dfeaa27bbe8e06d
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-17 17:01:58 +0500

    gradient-with-draggables.jsx: ➕ Create

---
+++ src/components/dash-gradient-with-draggables/gradient-with-draggables.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Draggable from './draggable.jsx';
+
+import styles from './gradient-with-draggables.css';
+import log from '../../log/log';
+
+const stopsToGradient = (stops, gradientType) => {
+    switch (gradientType) {
+    case GradientTypes.HORIZONTAL:
+        return (
+            <linearGradient id="gradient-with-draggables-swatch-gradient">
+                {stops.map((stop, index) => (
+                    <stop
+                        key={index}
+                        offset={stop.offset}
+                        stopColor={stop.color}
+                    />
+                ))}
+            </linearGradient>
+        );
+    case GradientTypes.RADIAL:
+        return (
+            <radialGradient id="gradient-with-draggables-swatch-gradient">
+                {stops.map((stop, index) => (
+                    <stop
+                        key={index}
+                        offset={stop.offset}
+                        stopColor={stop.color}
+                    />
+                ))}
+            </radialGradient>
+        );
+    default:
+        log.error(`Unrecognized gradient type: ${gradientType}`);
+        return null;
+    }
+};
+
+const GradientWithDraggablesComponent = props => (
+    <>
+        <svg
+            className={styles.gradientSwatch}
+            viewBox="0,0,192,60"
+        >
+            <defs>
+                {stopsToGradient(props.stops, props.gradientType)}
+            </defs>
+            <rect
+                width="192"
+                height="60"
+                fill="url(#gradient-with-draggables-swatch-gradient)"
+            />
+        </svg>
+        <div className={styles.draggablesBox}>
+            {props.stops.map((stop, index) => (
+                <Draggable
+                    key={index}
+                    onMoveStopPointerDown={e => props.onMoveStopPointerDown(e, index)}
+                    onSelectColor={() => props.onSelectColor(index)}
+                    stop={stop}
+                />
+            ))}
+        </div>
+    </>
+);
+
+GradientWithDraggablesComponent.propTypes = {
+    gradientType: PropTypes.oneOf(Object.keys(GradientTypes)).isRequired,
+    onMoveStopPointerDown: PropTypes.func.isRequired,
+    onSelectColor: PropTypes.func.isRequired,
+    stops: PropTypes.arrayOf(PropTypes.shape({
+        color: PropTypes.string,
+        offset: PropTypes.number
+    }))
+};
+
+export default GradientWithDraggablesComponent;
